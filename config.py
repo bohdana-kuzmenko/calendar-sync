b241d071addfa763af4aaaa5c4b343985555dddf
bogdana <bohdana.kuzmenko.work@gmail.com>
2019-12-26 16:21:11 +0200

    Update redirect url

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ other secure storage mechanism.
 
 CLIENT_ID = '6bc89158-5ce6-4754-a5a5-25a0952db9e1'
 CLIENT_SECRET = 'Dn_xfwNNhf3m8Gbu=rkn7o.XU/z6Pzq]'
-REDIRECT_URI = 'http://localhost:5000/login/authorized'
+REDIRECT_URI = 'https://fourth-vehicle-251408.appspot.com/login/authorized'
 AUTHORITY_URL = 'https://login.microsoftonline.com/common'
 AUTH_ENDPOINT = '/oauth2/v2.0/authorize'
 TOKEN_ENDPOINT = '/oauth2/v2.0/token'
